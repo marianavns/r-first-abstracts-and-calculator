6bcae59b6585cff8349197386b8b84533ce52aa9
marianavns <mariana.vns@gmail.com>
2021-02-12 11:53:08 -0300

    feat(R-archives): adiciona função somar

---
+++ R/hello.R
@@ -0,0 +1,16 @@
+#' Function somar
+#'
+#' This function is used to add two values.
+#'
+#' @param a first number.
+#' @param b another number.
+#'
+#' @examples
+#' sum(100, 50)
+#'
+#' Console response: 150
+#' @export
+somar <- function(a,b) {
+  a + b
+  # Could be sum(a,b) too. sum is a R native function.
+}
